d91c9d85322d27fce80e814c07e886f7b2fe3014
charlieliu <liuchangli0107@gmail.com>
2013-12-30 16:30:46 +0800

    練習 刪除DB(註解)

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -20,65 +20,141 @@ class Welcome extends CI_Controller {
 		$this->load->library('parser');
 
 		// 取得預設SESSION資料
-		$session_id = $this->session->userdata('session_id') ;
-		$ip_address = $this->session->userdata('ip_address') ;
-		$user_agent = $this->session->userdata('user_agent') ;
-		$last_activity = $this->session->userdata('last_activity') ;
-		$user_data = $this->session->userdata('user_data') ;
+		$session_id = $this->session->userdata('session_id') ; // CI session ID
+		$ip_address = $this->session->userdata('ip_address') ; // 使用者IP位置
+		$user_agent = $this->session->userdata('user_agent') ; // 使用者瀏覽器類型
+		$last_activity = $this->session->userdata('last_activity') ; // 最後變動時間
+		$user_data = $this->session->userdata('user_data') ; // 自訂資料
 
 		// DB測試
-		$test_ary = array(
+		
+		// 更新 SESSION_LOGS
+		//$this->_delete_old_session() ;
+
+
+		// 目前SESSION資料
+		$SESSION_LOGS = array(
            'SESSION_ID'		=> $session_id ,
            'IP_ADDRESS'		=> $ip_address ,
            'USER_AGENT'		=> $user_agent 
         );
 
 		// 取得存在DB個數
-		$test_query = $this->db->get_where('TEST_ARY', $test_ary, 1) ;
-		$count_num = $test_query->num_rows() ;
+		$query = $this->db->get_where('SESSION_LOGS', $SESSION_LOGS) ;
+		$count_num = $query->num_rows() ;
         
 		
 		if( $count_num == 0 )
 		{
-			// 'NOW()' 強制CI不處理
-			$this->db->set('ADDTIME', 'NOW()',false);
-			// 新增
-			$this->db->insert('TEST_ARY', $test_ary) ;
+			$this->db->set('ADDTIME', 'NOW()',false);// 'NOW()' 強制CI不處理
+			// CI 新增用法
+			$this->db->insert('SESSION_LOGS', $SESSION_LOGS) ;
 			// 更新目前資料庫查詢數量
-			$test_query = $this->db->get_where('TEST_ARY', $test_ary, 1) ;
-			$count_num = $test_query->num_rows() ;
+			$query = $this->db->get_where('SESSION_LOGS', $SESSION_LOGS, 1) ;
+			$count_num = $query->num_rows() ;
 		}
 		else
 		{
+			$this->db->set('UPDATETIME', 'NOW()',false);// 'NOW()' 強制CI不處理
+			// CI 更新用法
 			$this->db->where('SESSION_ID', $session_id);
-			// 'NOW()' 強制CI不處理
-			$this->db->set('UPDATETIME', 'NOW()',false);
-			$this->db->update('TEST_ARY');
+			$this->db->update('SESSION_LOGS');
 		}
-
-        //$this->db->_error_message();
         
 
+		// SESSION_LOGS
+		$SESSION_LOGS['SESSION_LOGS'] = print_r($SESSION_LOGS, true); // 最新資料筆數
+
+
+		// ci_sessions
+		$ci_sessions = array(
+			'session_id' => $session_id,
+			'ip_address' => $ip_address,
+			'user_agent' => $user_agent,
+			'last_activity' => $last_activity,
+			'user_data' => $user_data,
+		);
 		// 顯示資料
-		$test_ary['COUNT_NUM'] = $count_num;
 		$content = '' ;
-		$content .= !empty($session_id)?'session_id : '.$session_id.'<br />':'' ;
-		$content .= !empty($ip_address)?'ip_address : '.$ip_address.'<br />':'' ;
-		$content .= !empty($user_agent)?'user_agent : '.$user_agent.'<br />':'' ;
-		$content .= !empty($last_activity)?'last_activity : '.$last_activity.'<br />':'' ;
-		$content .= !empty($user_data)?'user_data : '.$user_data.'<br />':'' ;
-		$content .= 'session_test : '.print_r($test_ary,true).'<br />' ;
+		$content .= 'ci_sessions : '.print_r($ci_sessions,true).'<br /><br />' ;
+		$content .= 'SESSION_LOGS : '.print_r($SESSION_LOGS,true).'<br /><br />' ;
 
 		// 標題 內容顯示
 		$data = array(
 			'title' => 'Welcome to CodeIgniter',
-			'current_page' => strtolower(__CLASS__),
-			'current_fun' => strtolower(__FUNCTION__),
+			'current_page' => strtolower(__CLASS__), // 當下類別
+			'current_fun' => strtolower(__FUNCTION__), // 當下function
 			'content' => $content 
 		);
 
 		// Template parser class
 		$this->parser->parse('welcome_view', $data);
 		
+	}
+
+	private function _delete_old_session()
+	{
+		// 找出存活SESSION
+		$isalive = array();
+		
+		$this->db->select('session_id');// 產生： SELECT session_id
+
+		// 一分鐘前的SESSION
+		$this->db->where('last_activity >', $this->session->userdata('last_activity')-60);	// 產生： WHERE last_activity > val
+		
+		$query = $this->db->get('ci_sessions');// 產生： SELECT session_id FROM mytable
+
+		// 找出無用SESSION
+		if( $query->num_rows() > 0 )
+		{
+			
+			foreach ($query->result() as $v)
+			{
+		    	$v2 = get_object_vars($v) ;// object 轉換成 string
+		    	$isalive[] = $v2['session_id'];
+			}
+
+			// 還存活的SESSION
+			$query = $this->db->or_where_not_in('SESSION_ID', $isalive);	// 產生： WHERE SESSION_ID NOT IN ('val1', 'val2', 'val3') 	
+			$query = $this->db->get('SESSION_LOGS');
+			$delete_num = 0 ;
+			if( $query->num_rows() > 0 )
+			{
+				// 取得 要刪除的SESSION ID
+				$query_result = $query->result();
+				$query_values = array();
+				foreach ($query->result() as $v)
+				{
+					$v = get_object_vars($v);// object 轉換成 string
+					$query_values[] = $v['SESSION_ID'] ;
+
+				}
+				//exit( 'LINE '.__LINE__.'<br />'.print_r($query_values, true) ) ;
+
+				foreach ($query_values as $v)
+				{
+				    
+			    	if( $delete_num == 0 )
+			    	{
+			    		//
+			    		$this->db->where('SESSION_ID', $v);
+			    	}
+			    	else
+			    	{
+			    		//
+			    		$this->db->or_where('SESSION_ID', $v);
+			    	}
+			    	$delete_num ++ ;
+				    
+				}
+				
+				$this->db->delete('SESSION_LOGS');
+			}
+
+		}
+		else
+		{
+			$this->db->delete('SESSION_LOGS');
+		}
 	}
 }
