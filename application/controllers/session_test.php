d467ba249bbd516c9f78b87a6d274375656668ae
charlieliu <liuchangli0107@gmail.com>
2014-03-06 18:14:53 +0800

    簡易瀏覽器判斷

--- application/controllers/session_test.php
+++ application/controllers/session_test.php
@@ -38,6 +38,8 @@ class Session_test extends CI_Controller {
 		$user_data = $this->session->userdata('user_data') ;// 自訂資料
 		//$user_data = $this->_str_replace(print_r($user_data,true)) ;
 		//$user_data = $this->session->all_userdata() ;
+		$UserAgent = $this->get_UserAgent() ;
+		$UserAgent_str = $UserAgent!=false ? $UserAgent['A'].' '.$UserAgent['N'] : 'false' ;
 
 		// ci_sessions
 		$ci_sessions = array(
@@ -46,6 +48,7 @@ class Session_test extends CI_Controller {
 			'user_agent' => $user_agent,
 			'last_activity' => $last_activity,
 			'user_data' => $user_data,
+			'UserAgent' => $UserAgent_str,
 		);
 
 
@@ -105,6 +108,114 @@ class Session_test extends CI_Controller {
 		$html_date['content_div'] = $content_div ;
 		$this->parser->parse('index_view', $html_date ) ;
 		
+	}
+
+	private function get_UserAgent(){
+		$output = array(
+			"O" => $_SERVER["HTTP_USER_AGENT"],
+			"A" => $_SERVER["HTTP_USER_AGENT"],	// 瀏覽器 種類
+			"N" => '',							// 瀏覽器 版本
+			"M"	=> '',							// 作業系統
+			"MO"	=> '',						// 作業系統
+			"S"	=> ' ',							// 作業系統
+		);
+		// 作業系統
+		if( strpos($_SERVER["HTTP_USER_AGENT"],'Android') ){
+			$output['M'] = 'Mobile';
+			$output['MO'] = 'Mobile';
+			$output['S'] = 'Android';
+		}else if( strpos($_SERVER["HTTP_USER_AGENT"],'BlackBerry') ){
+			$output['M'] = 'Mobile';
+			$output['MO'] = 'Mobile';
+			$output['S'] = 'BlackBerry';
+		}else if( strpos($_SERVER["HTTP_USER_AGENT"],'iPhone') || strpos($_SERVER["HTTP_USER_AGENT"],'ipod') || strpos($_SERVER["HTTP_USER_AGENT"],'ipad') ){
+			$output['M'] = 'Mobile';
+			$output['MO'] = 'Mobile';
+			$output['S'] = 'iPhone';
+		}else if( strpos($_SERVER["HTTP_USER_AGENT"],'Palm') ){
+			$output['M'] = 'Mobile';
+			$output['MO'] = 'Mobile';
+			$output['S'] = 'Palm';
+		}else if( strpos($_SERVER["HTTP_USER_AGENT"],'Linux') ){
+			$output['M'] = 'Desktop';
+			$output['MO'] = 'Desktop';
+			$output['S'] = 'Linux';
+		}else if( strpos($_SERVER["HTTP_USER_AGENT"],'Macintosh') ){
+			$output['M'] = 'Desktop';
+			$output['MO'] = 'Desktop';
+			$output['S'] = 'Macintosh';
+		}else if( strpos($_SERVER["HTTP_USER_AGENT"],'Windows') ){
+			$output['M'] = 'Desktop';
+			$output['MO'] = 'Desktop';
+			$output['S'] = 'Windows';
+		}
+		// 瀏覽器
+		if(strpos($_SERVER["HTTP_USER_AGENT"],"MSIE 9.0") !== false){
+			$output['A'] = "Internet Explorer";
+			$output['N'] = "9";
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"MSIE 8.0") !== false){
+			$output['A'] = "Internet Explorer";
+			$output['N'] = "8";
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"MSIE 7.0") !== false){
+			$output['A'] = "Internet Explorer";
+			$output['N'] = "7";
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"MSIE 6.0") !== false){
+			$output['A'] = "Internet Explorer";
+			$output['N'] = "6";
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"MSIE") !== false){
+			$output['A'] = "Internet Explorer";
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"Firefox") !== false){
+			$output['A'] = "Firefox";
+			$str = $_SERVER["HTTP_USER_AGENT"] ;
+			$sit_0 = stripos($str,'Firefox/') + 8;
+			$str = substr($str,$sit_0) ;
+			$sit_1 = stripos($str,' ');
+			if($sit_1!==false)
+			{
+				$output['N'] = substr($str,0,$sit_1) ;
+			}
+			else
+			{
+				$output['N'] = $str ;
+			}
+			//$output['N'] = $sit_0.'/'.$sit_1.'/'.$sit_2 ;;
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"Chrome") !== false){
+			$output['A'] = "Chrome";
+			$str = $_SERVER["HTTP_USER_AGENT"] ;
+			$sit_0 = stripos($str,'Chrome/') + 7;
+			$str = substr($str,$sit_0) ;
+			$sit_1 = stripos($str,' ');
+			if($sit_1!==false)
+			{
+				$output['N'] = substr($str,0,$sit_1) ;
+			}
+			else
+			{
+				$output['N'] = $str ;
+			}
+			//$output['N'] = $sit_0.'/'.$sit_1.'/'.$sit_2 ;
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"Safari") !== false){
+			$output['A'] = "Safari";
+			$str = $_SERVER["HTTP_USER_AGENT"] ;
+			$sit_0 = stripos($str,'Version/') + 8;
+			$str = substr($str,$sit_0) ;
+			$sit_1 = stripos($str,' ');
+			if($sit_1!==false)
+			{
+				$output['N'] = substr($str,0,$sit_1) ;
+			}
+			else
+			{
+				$output['N'] = $str ;
+			}
+			//$output['N'] = $sit_0.'/'.$sit_1.'/'.$sit_2 ;
+		}else if(strpos($_SERVER["HTTP_USER_AGENT"],"Opera") !== false){
+			$output['A'] = "Opera";
+		}else if (strpos($_SERVER['HTTP_USER_AGENT'],"rv:11.0") !== false) {
+			$output['A'] = "Internet Explorer";
+			$output['N'] = "11";
+		}
+		return $output;
 	}
 
 	private function _str_replace($str){
