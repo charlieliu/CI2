24966db5afa04e84dcbaa06585281f5296838656
charlieliu <liuchangli0107@gmail.com>
2014-09-18 14:25:25 +0800

    css hover

--- application/controllers/file_upload.php
+++ application/controllers/file_upload.php
@@ -105,13 +105,27 @@ class File_upload extends CI_Controller {
             else
             {
                 $data = $this->upload->data();
+
+                // to re-size for thumbnail images un-comment and set path here and in json array
+                $config = array();
+                $config['image_library'] = 'gd2';
+                $config['source_image'] = $data['full_path'];
+                $config['create_thumb'] = TRUE;
+                $config['new_image'] = $data['file_path'] . 'thumbs/';
+                $config['maintain_ratio'] = TRUE;
+                $config['thumb_marker'] = '';
+                $config['width'] = 75;
+                $config['height'] = 50;
+                $this->load->library('image_lib', $config);
+                $this->image_lib->resize();
+
                 //set the data for the json array
                 $info->name = $data['file_name'];
                 $info->size = $data['file_size'];
                 $info->type = $data['file_type'];
                 $info->url = $upload_path_url . $data['file_name'];
                 // I set this to original file since I did not create thumbs.  change to thumbnail directory if you do = $upload_path_url .'/thumbs' .$data['file_name']
-                $info->thumbnailUrl = $upload_path_url . $data['file_name'];
+                $info->thumbnailUrl = $upload_path_url . 'thumbs/' . $data['file_name'];
                 $info->deleteUrl = base_url() . 'file_upload/deleteImage/' . $data['file_name'];
                 $info->deleteType = 'DELETE';
                 $info->error = null;
@@ -136,7 +150,7 @@ class File_upload extends CI_Controller {
 
     public function deleteImage($file) {//gets the job done but you might want to add error checking and security
         $success = unlink(FCPATH . 'uploads/' . $file);
-        //$success = unlink(FCPATH . 'uploads/thumbs/' . $file);
+        $success = unlink(FCPATH . 'uploads/thumbs/' . $file);
         //info to see if it is doing what it is supposed to
         $info->sucess = $success;
         $info->path = base_url() . 'uploads/' . $file;
--- application/views/file_upload_view.php
+++ application/views/file_upload_view.php
@@ -82,7 +82,7 @@
         <td>
             <span class="preview">
                 {% if (file.thumbnailUrl) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}" width="80" height="80"></a>
+                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
                 {% } %}
             </span>
         </td>
