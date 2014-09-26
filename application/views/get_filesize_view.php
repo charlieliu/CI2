b41c2af480da26f6c49782337298f6df4bc4596c
charlieliu <liuchangli0107@gmail.com>
2014-09-26 18:08:47 +0800

    total size

--- application/views/get_filesize_view.php
+++ application/views/get_filesize_view.php
@@ -1,57 +1,83 @@
 <div id="body">
     <p>{current_page}/{current_fun}</p>
 
-    <input id="fileupload" type="file">
-    <div>File Size:<span id="filesize">0kb</span></div>
-    <div id="err_msg"></div>
+    <div style="margin:1em;">
+        <input class="fileupload" type="file">
+        <div>File Size:<span>0 bytes</span></div>
+    </div>
+
+    <div style="margin:1em;">
+        <input class="fileupload" type="file">
+        <div>File Size:<span>0 bytes</span></div>
+    </div>
+
+    <div style="margin:1em;">
+        <input class="fileupload" type="file">
+        <div>File Size:<span>0 bytes</span></div>
+    </div>
+
+    <div style="margin:1em;">
+        <div>Total Size:<span id="totalsize">0 bytes</span></div>
+    </div>
 </div>
 
 <script type="text/javascript">
     <!--
     if( $.support.leadingWhitespace )
     {
-        $('#fileupload').change(function(){
-            if( this.files[0]!=null && 'number'==typeof(this.files[0].size))
+        function getFileSize(obj)
+        {
+            if( obj.files[0]!=null && 'number'==typeof(obj.files[0].size))
+                return obj.files[0].size;
+            else
+                return 0;
+        }
+        function getFormate(bytes)
+        {
+            if( bytes<1024 )
             {
-                var filesize;
-                if( this.files[0].size<1024 )
-                {
-                    filesize = this.files[0].size+' bytes';
-                }
-                else if( this.files[0].size<(1024*1024) )
-                {
-                    filesize = (this.files[0].size/1024).toFixed(2)+' kb';
-                }
-                else if( this.files[0].size<(1024*1024*1024) )
-                {
-                    filesize = (this.files[0].size/(1024*1024)).toFixed(2)+' MB';
-                }
-                else
-                {
-                    filesize = (this.files[0].size/(1024*1024*1024)).toFixed(2)+' GB';
-                }
+                return bytes + ' bytes';
+            }
+            else if( bytes<(1024*1024) )
+            {
+                return (bytes/1024).toFixed(2) + ' kb';
+            }
+            else if( bytes<(1024*1024*1024) )
+            {
+                return (bytes/(1024*1024)).toFixed(2) + ' MB';
             }
             else
             {
-                filesize = '0kb';
+                return (bytes/(1024*1024*1024)).toFixed(2) + ' GB';
             }
-            $('#filesize').html(filesize);
-            filesize = null;// 釋放IE記憶體
+        }
+        function getTotalSize()
+        {
+            var totalsize = 0;
+            $('.fileupload').each(function(){
+                totalsize += getFileSize(this);
+            });
+            $('#totalsize').html(getFormate(totalsize));
+        }
+        $('.fileupload').change(function(){
+            $(this).siblings('div').find('span').html(getFormate(getFileSize(this)));
+            getTotalSize();
         });
     }
     else
     {
-        var fileobj = ['Browser do not support'], v, count=0;
         /*
+        var fileobj = ['Browser do not support'], v, count=0;
         for(var n in this )
         {
             v = $('#filesize').n;
             fileobj.push('{" '+n+'" : "'+v+'"}');
             count++;
         }
-        */
         $('#err_msg').html(fileobj.join('<br>'));
         fileobj = null;// 釋放IE記憶體
+        */
+        $(this).siblings('div').find('span').html('Browser do not support');
     }
     -->
 </script>
