17a15a7f8eb91739743a01a646622a0b0c3f798c
charlieliu <liuchangli0107@gmail.com>
2014-11-06 11:31:24 +0800

    show js code

--- application/views/jquery_test/jquery_test_2_view.php
+++ application/views/jquery_test/jquery_test_2_view.php
@@ -2,11 +2,25 @@
     <p>{current_page}/{current_fun}</p>
 
     <div class="content_block">
-        <input id="check1" type="checkbox" checked="checked">
+        <input id="check1" type="checkbox">
         <label for="check1">Check me</label>
         <p id="show_me1"></p>
         <button id="attr_me">attr_me</button>
         <button id="prop_me">prop_me</button>
+    </div>
+
+    <div>
+        $('#attr_me').click(function(){<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;if( $("#check1").attr( "disabled" )!=undefined ) $("#check1").removeAttr("disabled").change();<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;else $("#check1").attr( "disabled", "disabled" ).change();<br>
+        });<br>
+    </div>
+
+    <div>
+        $('#prop_me').click(function(){<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;if( $("#check1").prop( "disabled" )==true ) $("#check1").prop("disabled",false).change();<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;else $("#check1").prop( "disabled", true ).change();<br>
+        });<br>
     </div>
 
     <script type="text/javascript">
@@ -19,8 +33,7 @@
                 ".prop( 'disabled' ): <b>" + $(this).prop( "disabled" ) + "</b><br>" +
                 ".is( ':disabled' ): <b>" + $(this).is( ":disabled" ) + "</b>"
             );
-        })
-        .change();
+        }).change();
 
         $('#attr_me').click(function(){
             if( $("#check1").attr( "disabled" )!=undefined )
