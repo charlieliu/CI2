f2075162f5d343307c8bc670121f98cfbfa5c203
charlieliu <liuchangli0107@gmail.com>
2014-12-08 14:52:11 +0800

    5. children html addClass text

--- application/views/jquery_test/jquery_append_view.php
+++ application/views/jquery_test/jquery_append_view.php
@@ -20,7 +20,8 @@
                 .append('<div class="test">1. Append</div>')
                 .prepend('<div class="test">2. Prepend</div>')
                 .before('<div class="test">3. Before</div>')
-                .after('<div class="test">4. After</div>');
+                .after('<div class="test">4. After</div>')
+                .children('div[class!="test"]').html('<div></div>').addClass('test').text('5. children html addClass text');
         });
     </script>
 </div>
