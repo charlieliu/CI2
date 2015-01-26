eb45d087b3edc6c9503a3e1ed8e5d5e2083196ca
charlieliu <liuchangli0107@gmail.com>
2015-01-26 17:25:06 +0800

    try null undefined in the array

--- application/views/jquery_test/jquery_test_9_view.php
+++ application/views/jquery_test/jquery_test_9_view.php
@@ -5,39 +5,52 @@
         <div id="show_info"></div>
         <script type="text/javascript">
             $(document).ready(function(){
-                var arr = ['1','2','3'], str = '';
+                var arr = [undefined,0,'1','2',3,null], str = '', res = '';
 
                 function show_arr(){
-                    str += 'var arr = [';
+                    str += 'var arr = [ ';
                     var num = 0;
                     for(var n in arr){
-                        if( num!=0 ) str += ',';
-                        str += '\''+arr[n]+'\'';
+                        if( num!=0 ) str += ', ';
+                        str += n+' : ('+typeof(arr[n])+')'+arr[n];
                         num++;
                     }
-                    str += '];<br>';
-                }
+                    str += ' ];<br>';
+                };
 
                 show_arr();
+
                 str += '<br>// push 從後面添加新元素 回傳元素個數<br>';
-                str += 'arr.push(\'push\') === '+arr.push('push')+'<br>';
                 show_arr();
-                str += 'arr.push(\'push\') === '+arr.push('push')+'<br>';
+                res = arr.push('push') ;
+                str += 'arr.push(\'push\') === ('+typeof(res)+')'+res+'<br>';
                 show_arr();
                 str += '<br>// pop 從後面拿掉元素 回傳拿掉元素<br>';
-                str += 'arr.pop() === '+arr.pop()+'<br>';
                 show_arr();
-                str += 'arr.pop() === '+arr.pop()+'<br>';
+                res = arr.pop() ;
+                str += 'arr.pop() === ('+typeof(res)+')'+res+'<br>';
+                show_arr();
+                res = arr.pop() ;
+                str += 'arr.pop() === ('+typeof(res)+')'+res+'<br>';
+                show_arr();
+                res = arr.pop() ;
+                str += 'arr.pop() === ('+typeof(res)+')'+res+'<br>';
                 show_arr();
                 str += '<br>// unshift 從前面添加新元素 回傳元素個數<br>';
-                str += 'arr.unshift(\'unshift\') === '+arr.unshift('unshift')+'<br>';
                 show_arr();
-                str += 'arr.unshift(\'unshift\') === '+arr.unshift('unshift')+'<br>';
+                res = arr.unshift('unshift') ;
+                str += 'arr.unshift(\'unshift\') === ('+typeof(res)+')'+res+'<br>';
                 show_arr();
                 str += '<br>// shift 從前面拿掉元素 回傳拿掉元素<br>';
-                str += 'arr.shift() === '+arr.shift()+'<br>';
                 show_arr();
-                str += 'arr.shift() === '+arr.shift()+'<br>';
+                res = arr.shift() ;
+                str += 'arr.shift() === ('+typeof(res)+')'+res+'<br>';
+                show_arr();
+                res = arr.shift() ;
+                str += 'arr.shift() === ('+typeof(res)+')'+res+'<br>';
+                show_arr();
+                res = arr.shift() ;
+                str += 'arr.shift() === ('+typeof(res)+')'+res+'<br>';
                 show_arr();
 
                 $('#show_info').html(str);
