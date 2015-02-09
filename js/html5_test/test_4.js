78f6cd30e50e0f02909e571dcd1d295da5b98e2b
charlieliu <liuchangli0107@gmail.com>
2015-02-09 14:07:38 +0800

    css for output

--- js/html5_test/test_4.js
+++ js/html5_test/test_4.js
@@ -2,7 +2,28 @@ $(document).ready(function(){
 
     $('#c').val($('#a').val());
 
-    $('#a').change(function(){$('#c').val(parseInt($('#a').val()))});
+    $('#x').css('padding','0').val(parseInt($('#a').val())+parseInt($('#b').val()));
+    $('#e').css('padding','0').val(parseInt($('#d').val()));
 
-    $('#d').change(function(){$('#g').val(parseInt($('#d').val())+parseInt($('#f').val()))});
+    $('#a').change(function(){
+        $('#c').val(parseInt($('#a').val()));
+    });
+    $('#b').change(function(){
+        $('#x').val(parseInt($('#a').val())+parseInt($('#b').val()));
+    });
+
+    $('#g').val(parseInt($('#d').val())+parseInt($('#f').val()));
+
+    $('#d').change(function(){
+        $('#g').val(parseInt($('#d').val())+parseInt($('#f').val()));
+    });
+    $('#f').change(function(){
+        $('#g').val(parseInt($('#d').val())+parseInt($('#f').val()));
+    });
+    $('#g').change(function(){
+        $('#d').val(parseInt($('#g').val())-parseInt($('#f').val()));
+        $('#e').val(parseInt($('#d').val()));
+    });
+
+
 });
