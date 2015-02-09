b09b41394afc11142ac728e176372b28b52ee891
charlieliu <liuchangli0107@gmail.com>
2015-02-09 17:24:30 +0800

    mroe info

--- application/views/html5_test/test_5_view.php
+++ application/views/html5_test/test_5_view.php
@@ -2,12 +2,12 @@
     <p>{current_page}/{current_fun}</p>
 
     <div class="content_block mg1em padding1em">
-        &lt;li data-animal-type="bird"&gt;Owl&lt;/li&gt;<br>
-        &lt;li data-animal-type="fish"&gt;Salmon&lt;/li&gt;<br>
-        &lt;li data-animal-type="spider"&gt;Tarantula&lt;/li&gt;<br>
+        &lt;li id="Owl" data-animal-type="bird" disabled="disabled"&gt;Owl&lt;/li&gt;<br>
+        &lt;li id="Salmon" data-animal-type="fish" clicked="clicked"&gt;Salmon&lt;/li&gt;<br>
+        &lt;li id="Tarantula" data-animal-type="spider"&gt;Tarantula&lt;/li&gt;<br>
         <ul>
-            <li id="Owl" data-animal-type="bird">Owl</li>
-            <li id="Salmon" data-animal-type="fish">Salmon</li>
+            <li id="Owl" data-animal-type="bird" disabled="disabled">Owl</li>
+            <li id="Salmon" data-animal-type="fish" clicked="clicked">Salmon</li>
             <li id="Tarantula" data-animal-type="spider">Tarantula</li>
         </ul>
     </div>
--- js/html5_test/test_5.js
+++ js/html5_test/test_5.js
@@ -6,11 +6,18 @@ $(document).ready(function(){
                 // this.attributes is not a plain object, but an array
                 // of attribute nodes, which contain both the name and value
                 if(this.specified) {
-                    console.log(this.name, this.value);
+                    //console.log(this.name, this.value);
                     str += this.name+' => ('+typeof(this.value)+')'+this.value+'<br>';
                 }
             });
         });
+        str += '<br>// attr() v.s. prop()<br>';
+        str += 'attr(\'id\') => ('+typeof($(this).attr('id'))+')'+$(this).attr('id')+'<br>';
+        str += 'attr(\'data-animal-type\') => ('+typeof($(this).attr('data-animal-type'))+')'+$(this).attr('data-animal-type')+'<br>';
+        str += 'attr(\'disabled\') => ('+typeof($(this).attr('disabled'))+')'+$(this).attr('disabled')+'<br>';
+        str += 'attr(\'clicked\') => ('+typeof($(this).attr('clicked'))+')'+$(this).attr('clicked')+'<br>';
+        str += 'prop(\'disabled\') => ('+typeof($(this).prop('disabled'))+')'+$(this).prop('disabled')+'<br>';
+        str += 'prop(\'clicked\') => ('+typeof($(this).prop('clicked'))+')'+$(this).prop('clicked')+'<br>';
         $('#results').html(str);
     });
 });
