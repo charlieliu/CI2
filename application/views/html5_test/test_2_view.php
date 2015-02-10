c448bf41b61dc1d3bc4d272848577f89cfb6e34a
charlieliu <liuchangli0107@gmail.com>
2015-02-10 11:27:12 +0800

    UI

--- application/views/html5_test/test_2_view.php
+++ application/views/html5_test/test_2_view.php
@@ -1,36 +1,36 @@
 <div id="body">
     <p>{current_page}/{current_fun}</p>
 
-    <div class="mg1em">
+    <div class="content_block mg1em padding1em">
         測試日期&nbsp;:&nbsp;{test_date}
-
-        {grid_view}
-
-        <form method="POST" class="contact1">
-            <div class="content_block mg1em padding1em">
-                &lt;input&nbsp;<span style="color:red;">type="range"</span>&nbsp;name="range" id="range" min="0" max="100" step="5" required&gt;<br>
-            </div>
-            <table class="mg1em" border="1" style="text-align:center;">
-                <tr><th>Chrome</th><th>Firefox</th><th>Safari</th><th>IE11</th><th>Opera</th></tr>
-                <tr><td>O</td><td>O</td><td>O</td><td>O</td><td>O</td></tr>
-            </table>
-            <div class="mg1em">
-                <input type="range" name="range" id="range" min="0" max="100" step="5" required>
-                <input type="submit" name="submit" value="Submit">
-            </div>
-            <div class="mg1em results"></div>
-        </form>
-
-        <form method="POST" class="contact1">
-            <div class="content_block mg1em padding1em">
-                &lt;input type="text" name="country_code"&nbsp;<span style="color:red;">pattern="[A-Za-z]{3}"</span>&nbsp;title="Three letter country code" required&gt;<br>
-                &lt;input&nbsp;<span style="color:red;">type="submit"</span>&nbsp;name="submit" value="Submit"&gt;
-            </div>
-            <div class="mg1em">
-                <input type="text" name="country_code" pattern="[A-Za-z]{3}" title="Three letter country code" required>
-                <input type="submit" name="submit" value="Submit">
-            </div>
-            <div class="mg1em results"></div>
-        </form>
     </div>
+
+    {grid_view}
+
+    <form method="POST" class="contact1">
+        <div class="content_block mg1em padding1em">
+            &lt;input&nbsp;<span style="color:red;">type="range"</span>&nbsp;name="range" id="range" min="0" max="100" step="5" required&gt;<br>
+        </div>
+        <table class="mg1em" border="1" style="text-align:center;">
+            <tr><th>Chrome</th><th>Firefox</th><th>Safari</th><th>IE11</th><th>Opera</th></tr>
+            <tr><td>O</td><td>O</td><td>O</td><td>O</td><td>O</td></tr>
+        </table>
+        <div class="mg1em">
+            <input type="range" name="range" id="range" min="0" max="100" step="5" required>
+            <input type="submit" name="submit" value="Submit">
+        </div>
+        <div class="mg1em results"></div>
+    </form>
+
+    <form method="POST" class="contact1">
+        <div class="content_block mg1em padding1em">
+            &lt;input type="text" name="country_code"&nbsp;<span style="color:red;">pattern="[A-Za-z]{3}"</span>&nbsp;title="Three letter country code" required&gt;<br>
+            &lt;input&nbsp;<span style="color:red;">type="submit"</span>&nbsp;name="submit" value="Submit"&gt;
+        </div>
+        <div class="mg1em">
+            <input type="text" name="country_code" pattern="[A-Za-z]{3}" title="Three letter country code" required>
+            <input type="submit" name="submit" value="Submit">
+        </div>
+        <div class="mg1em results"></div>
+    </form>
 </div>
