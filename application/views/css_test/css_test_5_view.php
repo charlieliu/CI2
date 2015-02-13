ca1aeeb85ee845c2543caaac90654452cb2a96be
charlieliu <liuchangli0107@gmail.com>
2015-02-13 17:02:36 +0800

    for linux browser

--- application/views/css_test/css_test_5_view.php
+++ application/views/css_test/css_test_5_view.php
@@ -19,48 +19,48 @@
     <div class="content_block">
         <h3>CSS3 2D translate(100px,100px)</h3>
         <ul id="translate">
-            <li><a href="#"><img src="images/j.jpg"/></a></li>
-            <li><a href="#"><img src="images/q.jpg"/></a></li>
-            <li><a href="#"><img src="images/u.jpg"/></a></li>
-            <li><a href="#"><img src="images/e.jpg"/></a></li>
-            <li><a href="#"><img src="images/r.jpg"/></a></li>
-            <li><a href="#"><img src="images/y.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/j.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/q.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/u.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/e.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/r.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/y.jpg"/></a></li>
         </ul>
     </div>
 
     <div class="content_block">
         <h3>CSS3 2D scale(2,2)</h3>
         <ul id="scale">
-            <li><a href="#"><img src="images/j.jpg"/></a></li>
-            <li><a href="#"><img src="images/q.jpg"/></a></li>
-            <li><a href="#"><img src="images/u.jpg"/></a></li>
-            <li><a href="#"><img src="images/e.jpg"/></a></li>
-            <li><a href="#"><img src="images/r.jpg"/></a></li>
-            <li><a href="#"><img src="images/y.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/j.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/q.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/u.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/e.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/r.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/y.jpg"/></a></li>
         </ul>
     </div>
 
     <div class="content_block">
         <h3>CSS3 2D skew(30deg,30deg)</h3>
         <ul id="skew">
-            <li><a href="#"><img src="images/j.jpg"/></a></li>
-            <li><a href="#"><img src="images/q.jpg"/></a></li>
-            <li><a href="#"><img src="images/u.jpg"/></a></li>
-            <li><a href="#"><img src="images/e.jpg"/></a></li>
-            <li><a href="#"><img src="images/r.jpg"/></a></li>
-            <li><a href="#"><img src="images/y.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/j.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/q.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/u.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/e.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/r.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/y.jpg"/></a></li>
         </ul>
     </div>
 
     <div class="content_block">
         <h3>CSS3 2D rotate(360deg)</h3>
         <ul id="rotate">
-            <li><a href="#"><img src="images/j.jpg"/></a></li>
-            <li><a href="#"><img src="images/q.jpg"/></a></li>
-            <li><a href="#"><img src="images/u.jpg"/></a></li>
-            <li><a href="#"><img src="images/e.jpg"/></a></li>
-            <li><a href="#"><img src="images/r.jpg"/></a></li>
-            <li><a href="#"><img src="images/y.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/j.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/q.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/u.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/e.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/r.jpg"/></a></li>
+            <li><a href="#"><img src="<?=base_url()?>images/y.jpg"/></a></li>
         </ul>
     </div>
 
