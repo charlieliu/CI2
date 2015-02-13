edd36783c241aef6fad57181f1b4b8c3f0bd7d96
charlieliu <liuchangli0107@gmail.com>
2015-02-13 16:35:33 +0800

    remark

--- application/views/css_test/css_test_5_view.php
+++ application/views/css_test/css_test_5_view.php
@@ -3,7 +3,8 @@
 
     <!-- CSS3 2D Transforms -->
     <div class="content_block">
-        <h3>CSS3 2D Transforms</h3>
+        <h3>CSS3 2D matrix(2,-0.5,1,2,0,0)</h3>
+        <p>P.S. matrix(scaleX(x), skewX(tanY), skewX(tanX), scaleX(Y), translate(X), translate(Y))</p>
         <ul id="abgne-block-20140616">
             <li><a href="#"><img src="<?=base_url()?>images/j.jpg"/></a></li>
             <li><a href="#"><img src="<?=base_url()?>images/q.jpg"/></a></li>
@@ -16,7 +17,7 @@
     <!-- CSS3 2D Transforms -->
 
     <div class="content_block">
-        <h3>CSS3 translate(100px,100px)</h3>
+        <h3>CSS3 2D translate(100px,100px)</h3>
         <ul id="translate">
             <li><a href="#"><img src="images/j.jpg"/></a></li>
             <li><a href="#"><img src="images/q.jpg"/></a></li>
@@ -28,7 +29,7 @@
     </div>
 
     <div class="content_block">
-        <h3>CSS3 scale(2,2)</h3>
+        <h3>CSS3 2D scale(2,2)</h3>
         <ul id="scale">
             <li><a href="#"><img src="images/j.jpg"/></a></li>
             <li><a href="#"><img src="images/q.jpg"/></a></li>
@@ -40,7 +41,7 @@
     </div>
 
     <div class="content_block">
-        <h3>CSS3 skew(30deg,30deg)</h3>
+        <h3>CSS3 2D skew(30deg,30deg)</h3>
         <ul id="skew">
             <li><a href="#"><img src="images/j.jpg"/></a></li>
             <li><a href="#"><img src="images/q.jpg"/></a></li>
@@ -52,7 +53,7 @@
     </div>
 
     <div class="content_block">
-        <h3>CSS3 rotate(360deg)</h3>
+        <h3>CSS3 2D rotate(360deg)</h3>
         <ul id="rotate">
             <li><a href="#"><img src="images/j.jpg"/></a></li>
             <li><a href="#"><img src="images/q.jpg"/></a></li>
