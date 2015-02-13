ba8984a2fa9e09595b7b4a1afcd118e609096f10
charlieliu <liuchangli0107@gmail.com>
2015-02-13 11:43:07 +0800

    #body min-height

--- application/views/html5_test/test_6_view.php
+++ application/views/html5_test/test_6_view.php
@@ -1,6 +1,5 @@
 <div id="body">
     <p>{current_page}/{current_fun}</p>
-
     <div id="div1" class="drap_div" ondrop="drop(event)" ondragover="allowDrop(event)">
         <img src="images/j.jpg" draggable="true" ondragstart="drag(event)" id="drag1" class="slide" />
         <img src="images/q.jpg" draggable="true" ondragstart="drag(event)" id="drag2" class="slide" />
