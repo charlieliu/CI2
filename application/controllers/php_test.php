aac68ef5db874f3d44cce1c10be4150f230b859a
charlieliu <liuchangli0107@gmail.com>
2015-03-30 17:19:43 +0800

    chek IP

--- application/controllers/php_test.php
+++ application/controllers/php_test.php
@@ -505,9 +505,13 @@ class Php_test extends CI_Controller {
             'content_value' => $this->_str_replace(print_r($SESSION_LOGS,true))
         ) ;
         */
+       $server_copy = $_SERVER ;
+       $server_copy['ip_check']['HTTP_CLIENT_IP'] = !empty($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : 'error : empty' ;
+       $server_copy['ip_check']['HTTP_X_FORWARDED_FOR'] = !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : 'error : empty' ;
+       $server_copy['ip_check']['REMOTE_ADDR'] = !empty($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'error : empty' ;
         $content[] = array(
             'content_title' => '_SERVER',
-            'content_value' => $this->_str_replace(print_r($_SERVER,true))
+            'content_value' => $this->_str_replace(print_r($server_copy,true))
         ) ;
 
         // 標題 內容顯示
