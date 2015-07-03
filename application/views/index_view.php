265ce45c96eeacee517cd7287376a153af75198f
charlieliu <liuchangli0107@gmail.com>
2015-07-03 13:53:22 +0800

    ???

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -7,7 +7,7 @@
     <meta name="description" content="{title}">
     <meta property="og:image" content="<?=base_url()?>images/joba.jpg">
     <meta http-equiv="X-Content-Type-Options" content="nosniff">
-    <meta http-equiv="x-frame-options" content="sameorigin">
+    <meta http-equiv="x-frame-options" content="SAMEORIGIN">
     <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
 
     <title>{title}</title>
